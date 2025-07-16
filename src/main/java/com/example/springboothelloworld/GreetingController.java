31e8ad4ec4a5034d1b2cef20a04c213d3ec3c9e3
DESKTOP-GR883KV\user <kwangun2067@gmail.com>
2025-07-16 18:22:24 +0900

    feat

--- src/main/java/com/example/springboothelloworld/GreetingController.java
+++ src/main/java/com/example/springboothelloworld/GreetingController.java
@@ -1,10 +1,12 @@
 package com.example.springboothelloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class GreetingController {
     private final GreetingService service;
 
